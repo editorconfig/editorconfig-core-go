3ec0d747375ae6ddb30842a14fb409d8b8a71406
Yoan Blanc <yoan@dosimple.ch>
2020-01-15 18:00:39 +0100

    cached parser
    Signed-off-by: Yoan Blanc <yoan@dosimple.ch>

---
+++ simple_parser.go
@@ -0,0 +1,31 @@
+package editorconfig
+
+import (
+	"os"
+
+	"gopkg.in/ini.v1"
+)
+
+type SimpleParser struct{}
+
+// ParseIni calls go-ini's Load on the file.
+func (parser *SimpleParser) ParseIni(filename string) (*Editorconfig, error) {
+	fp, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	defer fp.Close()
+
+	iniFile, err := ini.Load(fp)
+	if err != nil {
+		return nil, err
+	}
+
+	return newEditorconfig(iniFile)
+}
+
+// FnmatchCase calls the module's FnmatchCase.
+func (parser *SimpleParser) FnmatchCase(selector string, filename string) (bool, error) {
+	return FnmatchCase(selector, filename)
+}
