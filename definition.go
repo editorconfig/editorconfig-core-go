93b20009932a815cdd52b12d84f49317e44ff1b4
Yoan Blanc <yoan@dosimple.ch>
2020-02-01 11:38:19 +0100

    fixup! cached: metrics
    Signed-off-by: Yoan Blanc <yoan@dosimple.ch>

--- definition.go
+++ definition.go
@@ -39,7 +39,7 @@ func (d *Definition) normalize() error {
 	d.IndentStyle = strings.ToLower(d.Raw["indent_style"])
 
 	trimTrailingWhitespace, ok := d.Raw["trim_trailing_whitespace"]
-	if ok && trimTrailingWhitespace != "unset" {
+	if ok && trimTrailingWhitespace != UnsetValue {
 		trim, err := strconv.ParseBool(trimTrailingWhitespace)
 		if err != nil {
 			return fmt.Errorf("trim_trailing_whitespace=%s is not an acceptable value. %s", trimTrailingWhitespace, err)
@@ -48,7 +48,7 @@ func (d *Definition) normalize() error {
 	}
 
 	insertFinalNewline, ok := d.Raw["insert_final_newline"]
-	if ok && insertFinalNewline != "unset" {
+	if ok && insertFinalNewline != UnsetValue {
 		insert, err := strconv.ParseBool(insertFinalNewline)
 		if err != nil {
 			return fmt.Errorf("insert_final_newline=%s is not an acceptable value. %s", insertFinalNewline, err)
@@ -58,7 +58,7 @@ func (d *Definition) normalize() error {
 
 	// tab_width from Raw
 	tabWidth, ok := d.Raw["tab_width"]
-	if ok && tabWidth != "unset" {
+	if ok && tabWidth != UnsetValue {
 		num, err := strconv.Atoi(tabWidth)
 		if err != nil {
 			return fmt.Errorf("tab_width=%s is not an acceptable value. %s", tabWidth, err)
@@ -93,12 +93,12 @@ func (d *Definition) merge(md *Definition) {
 	if len(d.EndOfLine) == 0 {
 		d.EndOfLine = md.EndOfLine
 	}
-	if trimTrailingWhitespace, ok := d.Raw["trim_trailing_whitespace"]; !ok || trimTrailingWhitespace != "unset" {
+	if trimTrailingWhitespace, ok := d.Raw["trim_trailing_whitespace"]; !ok || trimTrailingWhitespace != UnsetValue {
 		if d.TrimTrailingWhitespace == nil {
 			d.TrimTrailingWhitespace = md.TrimTrailingWhitespace
 		}
 	}
-	if insertFinalNewline, ok := d.Raw["insert_final_newline"]; !ok || insertFinalNewline != "unset" {
+	if insertFinalNewline, ok := d.Raw["insert_final_newline"]; !ok || insertFinalNewline != UnsetValue {
 		if d.InsertFinalNewline == nil {
 			d.InsertFinalNewline = md.InsertFinalNewline
 		}
@@ -115,62 +115,66 @@ func (d *Definition) merge(md *Definition) {
 func (d *Definition) InsertToIniFile(iniFile *ini.File) {
 	iniSec := iniFile.Section(d.Selector)
 	for k, v := range d.Raw {
-		if k == "insert_final_newline" {
+		switch k {
+		case "insert_final_newline":
 			if d.InsertFinalNewline != nil {
-				iniSec.NewKey(k, strconv.FormatBool(*d.InsertFinalNewline))
+				v = strconv.FormatBool(*d.InsertFinalNewline)
 			} else {
 				insertFinalNewline, ok := d.Raw["insert_final_newline"]
-				if ok {
-					iniSec.NewKey(k, strings.ToLower(insertFinalNewline))
+				if !ok {
+					break
 				}
+				v = strings.ToLower(insertFinalNewline)
 			}
-		} else if k == "trim_trailing_whitespace" {
+		case "trim_trailing_whitespace":
 			if d.TrimTrailingWhitespace != nil {
-				iniSec.NewKey(k, strconv.FormatBool(*d.TrimTrailingWhitespace))
+				v = strconv.FormatBool(*d.TrimTrailingWhitespace)
 			} else {
 				trimTrailingWhitespace, ok := d.Raw["trim_trailing_whitespace"]
-				if ok {
-					iniSec.NewKey(k, strings.ToLower(trimTrailingWhitespace))
+				if !ok {
+					break
 				}
+				v = strings.ToLower(trimTrailingWhitespace)
 			}
-		} else if k == "charset" {
-			iniSec.NewKey(k, d.Charset)
-		} else if k == "end_of_line" {
-			iniSec.NewKey(k, d.EndOfLine)
-		} else if k == "indent_style" {
-			iniSec.NewKey(k, d.IndentStyle)
-		} else if k == "tab_width" {
+		case "charset":
+			v = d.Charset
+		case "end_of_line":
+			v = d.EndOfLine
+		case "indent_style":
+			v = d.IndentStyle
+		case "tab_width":
 			tabWidth, ok := d.Raw["tab_width"]
-			if ok && tabWidth == "unset" {
-				iniSec.NewKey(k, tabWidth)
+			if ok && tabWidth == UnsetValue {
+				v = tabWidth
 			} else {
-				iniSec.NewKey(k, strconv.Itoa(d.TabWidth))
+				v = strconv.Itoa(d.TabWidth)
 			}
-		} else if k == "indent_size" {
-			iniSec.NewKey(k, d.IndentSize)
-		} else {
-			iniSec.NewKey(k, v)
+		case "indent_size":
+			v = d.IndentSize
 		}
+
+		iniSec.NewKey(k, v) // nolint: errcheck
 	}
 
 	if _, ok := d.Raw["indent_size"]; !ok {
 		tabWidth, ok := d.Raw["tab_width"]
-		if ok && tabWidth == "unset" {
+		switch {
+		case ok && tabWidth == UnsetValue:
 			// do nothing
-		} else if d.TabWidth > 0 {
-			iniSec.NewKey("indent_size", strconv.Itoa(d.TabWidth))
-		} else if d.IndentStyle == IndentStyleTab && (d.version == nil || d.version.GTE(v0_9_0)) {
-			iniSec.NewKey("indent_size", IndentStyleTab)
+		case d.TabWidth > 0:
+			iniSec.NewKey("indent_size", strconv.Itoa(d.TabWidth)) // nolint: errcheck
+		case d.IndentStyle == IndentStyleTab && (d.version == nil || d.version.GTE(v0_9_0)):
+			iniSec.NewKey("indent_size", IndentStyleTab) // nolint: errcheck
 		}
 	}
 
 	if _, ok := d.Raw["tab_width"]; !ok {
-		if d.IndentSize == "unset" {
-			iniSec.NewKey("tab_width", d.IndentSize)
+		if d.IndentSize == UnsetValue {
+			iniSec.NewKey("tab_width", d.IndentSize) // nolint: errcheck
 		} else {
 			_, err := strconv.Atoi(d.IndentSize)
 			if err == nil {
-				iniSec.NewKey("tab_width", d.Raw["indent_size"])
+				iniSec.NewKey("tab_width", d.Raw["indent_size"]) // nolint: errcheck
 			}
 		}
 	}
